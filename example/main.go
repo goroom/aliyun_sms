ca5f56c0f1bf733cd73a04f16404bc14aa6be193
庞晓楠 <252228180@qq.com>
2017-01-03 09:30:49 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func main() {
-	aliyun_sms, err := aliyun_sms.NewAliyunSms("巨灵易停", "SMS_27880074", "LTAIR8r5E68vBuaN", "bSduBztakdjOuEq5HF0199tBgfxkXM")
+	aliyun_sms, err := aliyun_sms.NewAliyunSms("****", "SMS_****", "LTA*****vBuaN", "bSduBzta************gfxkXM")
 	if err != nil {
 		logger.Error(err)
 		return
 	}
-	err = aliyun_sms.Send("13319257173", `{"VerifyCode":"1234","Minutes":"30"}`)
+	err = aliyun_sms.Send("13319****73", `{"VerifyCode":"1234","Minutes":"30"}`)
 	if err != nil {
 		logger.Error(err)
 		return
