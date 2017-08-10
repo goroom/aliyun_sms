2fdf855feecd431f2f06cf033ddcba233336621a
Fengxq2014 <zxcasd555888@gmail.com>
2017-08-10 12:47:18 +0800

    '适配云通信2017-05-25api'

--- aliyun_sms.go
+++ aliyun_sms.go
@@ -1,9 +1,9 @@
 package aliyun_sms
 
 import (
+	"strings"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -32,21 +32,21 @@ func NewAliyunSms(sign_name string, template_code string, access_key_id string, 
 func (this *AliyunSms) Send(numbers string, params string) error {
 	var request Request
 	request.Format = "JSON"
-	request.Version = "2016-09-27"
+	request.Version = "2017-05-25"
 	request.AccessKeyId = this.AccessKeyID
 	request.SignatureMethod = "HMAC-SHA1"
 	request.Timestamp = time.Now().UTC().Format("2006-01-02T15:04:05Z")
 	request.SignatureVersion = "1.0"
 	request.SignatureNonce = rand.String(16, rand.RST_NUMBER|rand.RST_LOWER)
 
-	request.Action = "SingleSendSms"
+	request.Action = "SendSms"
 	request.SignName = this.SignName
 	request.TemplateCode = this.TemplateCode
-	request.RecNum = numbers
-	request.ParamString = params
+	request.PhoneNumbers = numbers
+	request.TemplateParam = params
+	request.RegionId = "cn-hangzhou"
 
 	url := request.ComposeUrl("GET", this.AccessSecret)
-	fmt.Println(url)
 	var resp *http.Response
 	var err error
 	resp, err = http.Get(url)
@@ -64,9 +64,9 @@ func (this *AliyunSms) Send(numbers string, params string) error {
 		return err
 	}
 	message, ok := _m["Message"]
-	if ok {
-		return errors.New(message)
+	if ok && strings.ToUpper(message) == "OK" {
+		return nil
 	}
 
-	return nil
+	return errors.New("send sms error")
 }
